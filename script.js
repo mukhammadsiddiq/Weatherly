304b6e44ffbf25b7961fba1c91e3944b9745a943
muxammad <ibrohimovmuhammad2020@gmail.com>
2024-07-09 10:46:22 +0300

    debugging

--- script.js
+++ script.js
@@ -247,14 +247,3 @@ locationBtn.addEventListener('click',getUserCoordinates);
 cityInput.addEventListener('keyup', e => e.key === 'Enter' && getCityCoordinates());
 window.addEventListener('load', getUserCoordinates)
 
-function sendMail(){
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        Subject: document.getElementById("Subject").value,
-        message: document.getElementById("message").value,
-    }
-
-    emailjs.send("service_j84k1q9","template_ivmybyt",parms).then(alert("Email was sent successfully"))
-}
-
